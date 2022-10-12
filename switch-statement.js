aeb6aea9a1ccebd9ebe17c9957f1c0f8f80fa804
Hồ Việt Đức <112045195+shinducql@users.noreply.github.com>
2022-10-12 11:46:44 +0700

    comit
    comit

---
+++ switch-statement.js
@@ -0,0 +1,13 @@
+var pass =50; // Pass mark
+var score = 90; //Score
+
+
+var hasPassed ='3' == 3;
+
+
+
+
+
+
+var el = document.getElementById('answer');
+el.textContent = 'Level passed: ' + hasPassed;
